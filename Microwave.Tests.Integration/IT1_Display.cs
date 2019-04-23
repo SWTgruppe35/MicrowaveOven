afd8e95c48f229310b4eb9d36d9ef6a9712b7197
Nichlaes <nichlaes.soerensen@gmail.com>
2019-04-23 13:41:54 +0200

    Added test for I1 Display and output

--- Microwave.Tests.Integration/IT1_Display.cs
+++ Microwave.Tests.Integration/IT1_Display.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using NUnit.Framework;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
 
 namespace Microwave.Tests.Integration
 {
+    [TestFixture]
     public class IT1_Display
     {
+        private IOutput outputSub;
+        private IDisplay display;
 
+        [SetUp]
+        public void SetUp()
+        {
+            outputSub = Substitute.For<IOutput>();
+            display = new Display(outputSub);
+        }
+
+        [Test]
+        public void ShowTimeIsCalledWithParameters()
+        {
+            display.ShowTime(10,10);
+
+            outputSub.Received(1).OutputLine($"Display shows: {10:D2}:{10:D2}");
+        }
+
+        [Test]
+        public void ShowPowerIsCalledWithParameters()
+        {
+            display.ShowPower(10);
+
+            outputSub.Received(1).OutputLine($"Display shows: {10} W");
+        }
+
+        [Test]
+        public void ClearIsCalled()
+        {
+            display.Clear();
+
+            outputSub.Received(1).OutputLine("Display cleared");
+        }
     }
 }
