0c5f0384bb0f7ee1497be4633223fa36daf736a0
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-04-23 15:59:10 +0200

    IT5_CookControllerTimer added + setup implemented

---
+++ Microwave.Tests.Integration/IT5_CookControllerTimer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Tests.Integration
+{
+    [TestFixture]
+    public class IT5_CookControllerTimer
+    {
+        private IOutput _outputSub;
+        private ITimer _timer;
+        private IUserInterface _userInterfaceSub;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ICookController _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _outputSub = Substitute.For<IOutput>();
+            _timer = new Timer();
+            _userInterfaceSub = Substitute.For<IUserInterface>();
+            _display = new Display(_outputSub);
+            _powerTube = new PowerTube(_outputSub);
+            _sut = new CookController(_timer, _display, _powerTube, _userInterfaceSub);
+        }
+
+    }
+}
