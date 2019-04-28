9839ecbbafdb7dbf694276833ac37a69ea4c8b6f
Berthelmaster <berthel0802@gmail.com>
2019-04-28 12:50:07 +0200

    Startcancelbot started

--- Microwave.Tests.Integration/IT8_StartCancelBottonUI.cs
+++ Microwave.Tests.Integration/IT8_StartCancelBottonUI.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace Microwave.Tests.Integration
 {
+    [TestFixture]
     class IT8_UserInterfaceDoor
     {
+        private IUserInterface _sut;
+        private IOutput _outputSub;
+        private IDoor _doorSub;
+        private IButton _powerButton;
+        private IButton _timeButtonSub;
+        private IButton _startCancelButtonSub;
+        private IDisplay _display;
+        private ILight _light;
+        private ICookController _cookControllerSub;
+
+        [SetUp]
+        public void Setup()
+        {
+            _outputSub = Substitute.For<IOutput>();
+            _light = new Light(_outputSub);
+            _display = new Display(_outputSub);
+            _doorSub = Substitute.For<IDoor>();
+            _powerButton = new Button();
+            _timeButtonSub = Substitute.For<IButton>();
+            _startCancelButtonSub = Substitute.For<IButton>();
+            _cookControllerSub = Substitute.For<ICookController>();
+            _sut = new UserInterface(_powerButton, _timeButtonSub, _startCancelButtonSub, _doorSub, _display, _light, _cookControllerSub);
+            _startCancelButtonSub.Press();
+
+        }
 
     }
 }
