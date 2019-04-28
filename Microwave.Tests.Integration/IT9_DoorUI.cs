e93035f13815d188191cf174438c85bc56c7e3ac
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-04-28 13:01:33 +0200

    IT9_DoorUI, setup made

--- Microwave.Tests.Integration/IT9_DoorUI.cs
+++ Microwave.Tests.Integration/IT9_DoorUI.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace Microwave.Tests.Integration
 {
+    [TestFixture()]
     class IT9_DoorUI
     {
+        private IUserInterface _sut;
+        private IButton _powerbtn;
+        private IButton _timerbtn;
+        private IButton _start_cancelbtn;
+        private IDisplay _display;
+        private ILight _light;
+        private IDoor _door;
+        private IOutput _outputSub;
+        private ICookController _controllerSub;
 
+        [SetUp]
+        public void setup()
+        {
+            _powerbtn = new Button();
+            _timerbtn = new Button();
+            _outputSub = Substitute.For<IOutput>();
+            _display = new Display(_outputSub);
+            _start_cancelbtn = new Button();
+            _light = new Light(_outputSub);
+            _door = new Door();
+            _controllerSub = Substitute.For<ICookController>();
+            _sut = new UserInterface(_powerbtn, _timerbtn, _start_cancelbtn, _door, _display, _light, _controllerSub);
+        }
     }
 }
