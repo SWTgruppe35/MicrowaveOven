5aa9ccab1b8db3f4af16e4b485c6d5e25339e16a
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-04-28 12:44:05 +0200

    TestCase for IT9_DoorUI created

---
+++ Microwave.Tests.Integration/IT9_DoorUI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave.Tests.Integration
+{
+    class IT9_DoorUI
+    {
+
+    }
+}
